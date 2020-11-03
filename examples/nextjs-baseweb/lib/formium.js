16e71fadc2faf5a03b12588248f8e03641722e0c
Jared Palmer <jared@palmer.net>
2020-11-03 15:23:00 -0500

    Remove base url

--- examples/nextjs-baseweb/lib/formium.js
+++ examples/nextjs-baseweb/lib/formium.js
@@ -2,5 +2,4 @@ import { createClient } from '@formium/client';
 
 export const formium = createClient(process.env.NEXT_PUBLIC_FORMIUM_PROJECTID, {
   apiToken: process.env.FORMIUM_TOKEN,
-  baseUrl: process.env.NEXT_PUBLIC_FORMIUM_API_URL,
 });
--- examples/nextjs-typescript/lib/formium.ts
+++ examples/nextjs-typescript/lib/formium.ts
@@ -1,6 +1,5 @@
-import { createClient } from '@formium/client';
+import { createClient } from '@formium/client'
 
 export const formium = createClient(process.env.NEXT_PUBLIC_FORMIUM_PROJECTID, {
   apiToken: process.env.FORMIUM_TOKEN,
-  baseUrl: process.env.NEXT_PUBLIC_FORMIUM_API_URL,
-});
+})
